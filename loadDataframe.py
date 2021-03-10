228df9189423c574c0825fc02707b0f3c486fd50
trebaxe <80334221+trebaxe@users.noreply.github.com>
2021-03-10 17:33:56 +0100

    Add files via upload

--- loadDataframe.py
+++ loadDataframe.py
@@ -19,7 +19,7 @@ schema = StructType([
 ])
 
 # Aanmaken van Dataframe
-    json_file_path = 'hdfs:///home/maria_dev/youtube8m/Youtube_Video_Dataset.json'
-    df = spark.read.json(json_file_path, schema, multiLine=True)
-    print(df.schema)
-    df.show()
+json_file_path = 'hdfs:///home/maria_dev/youtube8m/Youtube_Video_Dataset.json'
+df = spark.read.json(json_file_path, schema, multiLine=True)
+print(df.schema)
+df.show()
